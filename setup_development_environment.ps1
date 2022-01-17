52aab24a7eff8813610f5edda7453646519a62a4
Berkan Uslu <>
2022-01-17 18:13:41 +0100

    UPDATED Program list.

--- setup_development_environment.ps1
+++ setup_development_environment.ps1
@@ -9,8 +9,9 @@ choco install ChocolateyGUI -y
 
 # core components
 Write-Host "====> Installing core components..."
+choco install cmder -y
 choco install powershell-core -y
-choco install microsoft-windows-terminal -y
+#choco install microsoft-windows-terminal -y
 choco install git -y
 choco install curl -y
 choco install wget -y
@@ -18,40 +19,44 @@ choco install nodejs-lts -y
 choco install python3 -y
 choco install cmake -y
 choco install 7zip -y
-choco install docker-desktop -y
-choco install kubernetes-cli -y
-choco install Minikube -y
-choco install docker-compose -y
 choco install docker-cli -y
+choco install kubernetes-cli -y
+# choco install minikube -y
+choco install docker-compose -y
+choco install docker-desktop -y
 choco install terraform -y
 choco install openssl -y
 choco install putty -y
 choco install jq -y
-choco install redis -y
-choco install redis-desktop-manager -y
-#choco install redistray -y
 choco install dotnetcore-sdk -y
+choco install lens -y
+choco install meld -y
+choco install azure-cli -y
+# choco install mongodb-compass-isolated -y
+# choco install microsoftazurestorageexplorer -y
 
 # git gui softwares
 Write-Host "====> Installing git gui softwares..."
 choco install git-fork -y
+choco install sublimemerge -y
 #choco install gitextensions -y
 
 # 3d development
 Write-Host "====> Installing 3D development softwares..."
 choco install blender -y
 choco install epicgameslauncher -y
-choco install freecad -y
+#choco install freecad -y
 choco install steam -y
-choco install unity -y
-choco install renderdoc -y
+#choco install unity -y
+# choco install renderdoc -y
+# choco install InkScape -y
 
 # browsers
 Write-Host "====> Installing web browsers..."
 choco install firefox -y
 choco install GoogleChrome -y
 choco install opera-gx -y
-#choco install microsoft-edge -y
+choco install microsoft-edge -y
 
 # text editors
 Write-Host "====> Installing text editors..."
@@ -79,21 +84,38 @@ Install-Module oh-my-posh -Scope CurrentUser -Force
 
 # additional softwares
 Write-Host "====> Installing additional softwares..."
+choco install anydesk.install -y
 #choco install adobereader -y
+choco install calibre -y
 choco install dbeaver -y
 #choco install foxitreader -y
+choco install filezilla -y
+choco install insomnia-rest-api-client -y
+choco install grammarly -y
 choco install hwmonitor -y
 choco install logitech-options -y
-choco install passwordSafe -y
-choco install postman -y
+choco install lghub -y
+#choco install passwordSafe -y
+# choco install postman -y
+choco install powertoys -y
+choco install protonvpn -y
 choco install slack -y
+choco install discord.install -y
+choco install drmemory -y
 choco install sourcetrail -y
 choco install spotify -y
 choco install sumatrapdf -y
 choco install treesizefree -y
 choco install vlc -y
 choco install vmware-workstation-player -y
-choco install drmemory -y
+choco install zoom -y
+
+# video/photo editing softwares
+Write-Host "====> Installing video/photo editing softwares..."
+choco install rawtherapee -y
+choco install shotcut.install -y
+choco install nomacs -y
+choco install picpick.portable -y
 
 # windows subsystem for linux
 Write-Host "====> Installing windows subsystem for linux..."
